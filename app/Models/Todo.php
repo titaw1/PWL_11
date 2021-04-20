df50605cdd701c6401814ee17153f80d4c061a3b
Tita Wijayanti <wijayantitita094@gmail.com>
2021-04-20 16:26:34 +0700

    Melengkapi definisi relasi dengan model user, melengkapi atribut fillable, menghidden informasi kolom user_id dan castin atribut done dengan boolean pada model Todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'todo',
+        'label',
+        'done',
+    ];
+
+    protected $hidden = [
+        'user_id',
+    ];
+
+    protected $casts = [
+        'done' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
